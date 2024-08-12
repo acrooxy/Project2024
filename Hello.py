eadaefd75edeb1377532d1880a76835435e6f1fe
acrooxy <danor1122@gmail.com>
2024-08-12 18:34:51 +0100

    update

--- Hello.py
+++ Hello.py
@@ -2,3 +2,17 @@ a = 5
 b = 2*a
 print(b)
 print(a)
+
+print(len('I am Damian'))
+mystring = 'Hello World'
+print(mystring[6:])
+print(mystring[::3])
+print(mystring[::-1])
+print(mystring.find('l'))
+name = 'sam This is a string'
+# name[0] = 'P' Not correct
+last_letters = name[1:]
+print(last_letters)
+last_letters='P' + last_letters
+print(last_letters)
+print(last_letters.split('i'))
