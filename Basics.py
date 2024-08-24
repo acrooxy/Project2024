17b3263d6dab25a18be2673a862272dbef5c7434
acrooxy <danor1122@gmail.com>
2024-08-24 11:50:25 +0100

    working on basics, Functions, Importing, Loops, Exam from Statements. Backup in case of crash, linux messing

--- Basics.py
+++ Basics.py
@@ -159,3 +159,149 @@ while x < 5:
     #if the leeter == 'a':
         #continue # and go back to the loop
         #break stops (breaks out) whole loop
+
+for num in range(0,10,3):
+    print(num)
+
+print(list(range(0,11,2)))
+
+index_count = 0
+
+for letter in 'abcde':
+    print('At index {} the letter is {}'.format(index_count, letter))
+    index_count += 1
+
+ind_count = 0
+word = 'elephant'
+for letter in word:
+    print('Letter {} is at index {} '.format(letter, ind_count))
+    ind_count += 1
+
+# Python ENUMERATE option
+
+word = 'Karynka Konop'
+for let in enumerate(word): # creating tuple and enumerating
+    print(let)
+
+# option 2
+word = 'Damianek'
+for ind, lett in enumerate(word): # creating tuple and enumerating
+    print('index: ', ind, 'letter: ', lett)
+
+# Zip Function
+# Zip function packing two seperate list into the one tuple
+# Can be done with more than 2 lists
+# Bare it in mind zip function will be able to zip only maximum same numbers at the lists. So if the lists are
+# [3,2,1], [1,2,4,6,7], [2,5,6,7,9,0,4,5] The maximum tuples output will be 3 elements in the lists
+my_listX1 = [1,2,3]
+my_listX2 = ['a','b','c']
+zip(my_listX1, my_listX2)
+print(zip(my_listX1, my_listX2)) # zip object saved and waiting for you at 0x7f468e56b040> memory block
+
+for itemX in zip(my_listX1, my_listX2):
+    print(itemX)
+
+print(list(zip(my_listX1, my_listX2))) #cast it into the list
+
+
+ # Dicts again
+
+'mykey' in {'mykey':345}
+# True
+
+d = {'mykey':345}
+print(345 in d.values())
+
+print('mykey' in d.keys())
+
+# min function
+mylistY = [10,20,30,40,50,100]
+print(max(mylistY)) #MAX
+print(min(mylistY)) #MIN
+
+# Importing function from library
+
+from random import shuffle # this function dont returning anything so yuo cant stick it to variable
+
+shuffle(mylistY)
+print(mylistY)
+
+from random import randint # choose random INT
+
+aY = randint(0,100)
+print(aY)
+
+# Input always accept everything as a string so I need to force it int(input) =
+
+# List Comprehension
+AWord = 'DamianNoCoTy'
+print(AWord)
+mylistA = [x for x in AWord] #appending letter by letter in a list ##ELEMENT FOR ELELENT IN A VARIABLE
+print(mylistA)
+
+mylistB = [num for num in range(0,16,3)]
+print(mylistB)
+
+mylistC = [num**2 for num in range(0,16,3)]
+print(mylistC)
+
+mylistD = [num**2 for num in range(0,16,3) if num%2 == 0] #MODULO # check if the num is even - parzyste
+print(mylistD)
+
+Celc = [36.6, 30, 39.2] # Calculator Celcius to Farenheit
+mylistE = [num*(9/5) + 32 for num in Celc]
+
+
+print('Farenheit = ', mylistE)
+
+farenheitA =  []#[32.0, 50.0, 68.0, 94.1]
+
+for temp in Celc:
+    farenheitA.append(( (9/5) * temp + 32))
+
+print(farenheitA)
+
+results = [x if x%2==0 else 'ODD' for x in range(0, 11)] # IF ELSE in comprehension, but not advicible
+print(results)
+
+# Nestet lists
+myListF = []
+
+for x in [2,4,6]:
+    for y in [100,200,300]:
+        myListF.append((x*y))
+print(myListF)
+
+# Comprahension now
+
+myListG = [x*y for x in [2,4,6] for y in [1,10,1000]]
+
+print(myListG)
+
+st = 'Print only the words that start with s in this sentence'
+stDone = st.split()
+# print(stDone)
+
+for word in stDone:
+    if word[0] == 's':
+        print(word)
+    else:
+        continue
+
+
+a = []
+for num in range(0,11):
+    a.append(num)
+print(a)
+
+stWord = st.split()
+tempnum = 0
+
+stWord = st.split()
+for word in stWord:
+    if len(word) %2==0:
+        print('word \'{}\' with letter number {} is Even'.format(word, len(word)))
+    else:
+        continue
+
+a
